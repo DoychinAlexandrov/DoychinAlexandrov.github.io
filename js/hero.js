9cd9d7caec7f710ff884d5cd597bedcc5b79cb5b
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2024-01-01 11:21:26 +0200

    add all

---
+++ js/hero.js
@@ -0,0 +1,13 @@
+// export const hero = document.querySelector('.op');
+// let isRed = false;
+
+// hero.addEventListener('click', function () {
+//     if (!isRed) {
+//         hero.style.color = 'red';
+//         isRed = true;
+//     } else {
+//         // Change back to the original color
+//         hero.style.color = ''; // Empty string removes inline style
+//         isRed = false;
+//     }
+// });
