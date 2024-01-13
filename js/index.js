d28b2570a3c0eb577eaa32dafad24db8ebb656de
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2024-01-13 10:23:53 +0200

    add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const header = document.querySelector('.header')
-fetch('/header.html')
+fetch('/headerr.html')
     .then(res => res.text())
     .then(data => {
         header.innerHTML = data
@@ -7,7 +7,7 @@ fetch('/header.html')
     })
 
 const footer = document.querySelector('.footer')
-fetch('/footer.html')
+fetch('/footerr.html')
     .then(res => res.text())
     .then(data => {
         footer.innerHTML = data
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const header = document.querySelector('.header')
-fetch('/header.html')
+fetch('/headerr.html')
     .then(res => res.text())
     .then(data => {
         header.innerHTML = data;
     })
 
 const footer = document.querySelector('.footer')
-fetch('/footer.html')
+fetch('/footerr.html')
     .then(res => res.text())
     .then(data => {
         footer.innerHTML = data
