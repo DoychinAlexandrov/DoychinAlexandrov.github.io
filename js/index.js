dec6fa3c3e912d8fc4f59c33cafb8a3d857a42e2
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2024-01-14 12:41:48 +0200

    add

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-const header = document.querySelector('.header')
-fetch('/header.html')
-    .then(res => res.text())
-    .then(data => {
-        header.innerHTML = data
+// const header = document.querySelector('.header')
+// fetch('/header.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         header.innerHTML = data
 
-    })
+//     })
 
-const footer = document.querySelector('.footer')
-fetch('/footer.html')
-    .then(res => res.text())
-    .then(data => {
-        footer.innerHTML = data
-    })
+// const footer = document.querySelector('.footer')
+// fetch('/footer.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         footer.innerHTML = data
+//     })
 
 const intro = document.querySelector('.intro')
 fetch('/intro.html')
--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-const header = document.querySelector('.header')
-fetch('/header.html')
-    .then(res => res.text())
-    .then(data => {
-        header.innerHTML = data;
-    })
+// const header = document.querySelector('.header')
+// fetch('/header.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         header.innerHTML = data;
+//     })
 
-const footer = document.querySelector('.footer')
-fetch('/footer.html')
-    .then(res => res.text())
-    .then(data => {
-        footer.innerHTML = data
-    })
+// const footer = document.querySelector('.footer')
+// fetch('/footer.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         footer.innerHTML = data
+//     })
 
 
 /*
